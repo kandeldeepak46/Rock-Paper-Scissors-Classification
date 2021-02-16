91fde6f755ee9ea46dbdd7caec2ca75371fd0be3
xps <kandeldeepak46@gmail.com>
2021-02-16 08:41:09 +0545

    MyKerasModel class introduced and new structures are added

--- image_classifier/classifier/network.py
+++ image_classifier/classifier/network.py
@@ -32,8 +32,11 @@ from tensorflow.keras.preprocessing.image import (
 )
 from tensorflow.keras.applications import vgg16, VGG16
 from tensorflow.keras.utils import plot_model
-
+from tensorflow.keras.applications.vgg16 import VGG16
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.vgg16 import preprocess_input
 from pipeline import MyImageDataGenerator
+from tensorflow import keras
 
 MODEL_SAVE_DIR = os.path.join(
     os.path.dirname(__file__),
@@ -113,6 +116,70 @@ class MyLenetArchitecture(object):
         self.model.save_weights(MODEL_SAVE_PATH)
 
 
+class MyKerasModels(object):
+    def __init__(self, width, height, image_channel, number_of_classes):
+        self.width = width
+        self.height = height
+        self.image_channel = image_channel
+        super(MyKerasModels, self).__init__()
+
+    def build_naive_model(self):
+        model = tf.keras.Sequential(
+            [
+                tf.keras.layers.Conv2D(
+                    32,
+                    (3, 3),
+                    activation="relu",
+                    input_shape=(self.width, self.height, self.image_channel),
+                ),
+                tf.keras.layers.MaxPooling2D(2, 2),
+                tf.keras.layers.Conv2D(32, (3, 3), activation="relu"),
+                tf.keras.layers.MaxPooling2D(2, 2),
+                tf.keras.layers.Flatten(),
+                tf.keras.layers.Dense(128, activation=tf.nn.relu),
+                tf.keras.layers.Dense(6, activation=tf.nn.softmax),
+            ]
+        )
+
+    def build_fcnn_from_vgg16(width, height, channels, num_class):
+
+        # first create a FCNN
+        base_model = vgg16.VGG16(
+            weights="imagenet", include_top=False, input_shape=(width, height, channels)
+        )
+        fcnn_model = Sequential()
+        for layer in base_model.layers:
+            fcnn_model.add(layer)
+        fcnn_model.add(
+            Conv2D(filters=4096, kernel_size=(7, 7), name="fc1", activation="relu")
+        )  # named to keras vgg16
+        fcnn_model.add(
+            Conv2D(filters=4096, kernel_size=(1, 1), name="fc2", activation="relu")
+        )  # named to keras vgg16
+        fcnn_model.add(
+            Conv2D(filters=1000, kernel_size=(1, 1), name="predictions")
+        )  # named to keras vg16
+        fcnn_model.add(Softmax4D(axis=-1, name="softmax"))
+
+        # now reshape dense layer weights to FCNN Kernel Weights
+        # First Fetch original VGG16 with Dense Layers
+        vgg_top = vgg16.VGG16(
+            weights="imagenet", include_top=True, input_shape=(width, height, channels)
+        )
+        for layer in fcnn_model.layers:
+            # apply reshape to only fc1, fc2 and predictions layers
+            if layer.name.startswith("fc") or layer.name.startswith("pred"):
+                orig_layer = vgg_top.get_layer(layer.name)
+                W, b = orig_layer.get_weights()  # get original weights and bias
+                ax1, ax2, previous_filter, n_filter = layer.get_weights()[0].shape
+                new_W = W.reshape(ax1, ax2, -1, n_filter)
+                layer.set_weights([new_W, b])
+        del base_model
+        del vgg_top
+        #     %reset base_model
+        return fcnn_model
+
+
 def main():
     model = MyLenetArchitecture(224, 224, 3, 3)
     return_model = model.build_lenet_model()
@@ -120,4 +187,5 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    # main()
+    print("my name is kandel deepak")
