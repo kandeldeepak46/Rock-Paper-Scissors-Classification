6a5692cc0ed358979b636b2e0bf48db1f343da94
kandeldeepak <kandeldeepak46@gmail.com>
2021-06-09 20:38:01 +0545

    setup file added

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='image_classifier',
+      version='1.0',
+      description='package for classifying image through machine learning and computer vision application',
+      author='Deepak Kandel',
+      author_email='kandeldeepak46@gmail.com',
+      packages=['distutils'],
+     )
